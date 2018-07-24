d84e220ac83535591f08168cc51be3f7cec2ef1f
jmdressner <jmdressner@gmail.com>
2018-07-24 09:59:44 -0700

    adding goodle maps api

---
+++ Capstone/Capstone/key.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone
+{
+    public partial class Key
+    {
+        const string GOOGLE_MAP_KEY = "";
+    }
+}
