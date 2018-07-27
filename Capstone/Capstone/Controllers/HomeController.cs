4773c62628cc07f7dd54dd63c843c685af2902a6
jmdressner <jmdressner@gmail.com>
2018-07-27 11:21:38 -0700

    can sent email on contact page

--- Capstone/Capstone/Controllers/HomeController.cs
+++ Capstone/Capstone/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Capstone.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -41,8 +46,52 @@ namespace Capstone.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Test Form";
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Contact(Mail vm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msz = new MailMessage();
+                    msz.From = new MailAddress(vm.Email);//Email which you are getting 
+                                                         //from contact us page 
+                    msz.To.Add("mariedressner@gmail.com");//Where mail will be sent 
+                    msz.Subject = vm.Subject;
+                    msz.Body = vm.Message;
+                    var Epassword = vm.EmailPassword;
+
+                    SmtpClient smtp = new SmtpClient();
+
+                    smtp.Host = "smtp.gmail.com";
+
+                    smtp.Port = 587;
+
+                    smtp.Credentials = new System.Net.NetworkCredential
+                    (vm.Email, Epassword);
+
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(msz);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for Contacting us ";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+
+            return View();
+        }
+        public ActionResult Error()
+        {
             return View();
         }
 
---
+++ Capstone/Capstone/Models/Mail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class Mail
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Email Password")]
+        public string EmailPassword { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
